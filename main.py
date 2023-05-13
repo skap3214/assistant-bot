eb81f11a746458cc140648a635acc3102f44278d
skap3214 <kapadiasoami@gmail.com>
2023-05-13 11:59:35 +0530

    added main file and req.txt

---
+++ main.py
@@ -0,0 +1,19 @@
+from langchain.chat_models import ChatAnthropic
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+API_KEY = "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"
+chat = ChatAnthropic()
+query = "Hi!"
+messages = [
+    HumanMessage(content=query)
+]
+print(chat(messages))
