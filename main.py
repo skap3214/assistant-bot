5edde7bf6d561f6369c1afc91720b3773a0cedbf
skap3214 <kapadiasoami@gmail.com>
2023-05-21 15:26:49 +0530

    gitignore changed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from langchain.chat_models import ChatAnthropic
+import api_keys as api
+from langchain.chat_models import openai
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
@@ -10,10 +11,13 @@ from langchain.schema import (
     HumanMessage,
     SystemMessage
 )
-OPENAI_API_KEY = "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"
-chat = ChatAnthropic()
-query = "Hi!"
-messages = [
-    HumanMessage(content=query)
-]
-print(chat(messages))
+
+
+assistant_prompt = '''You are an assistant to a human, powered by a large language model trained by OpenAI.
+
+You are designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, you are able to generate human-like text based on the input you receive, allowing you to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+You are constantly learning and improving, and your capabilities are constantly evolving. You are able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. You have access to some personalized information provided by the human in the Context section below. Additionally, you are able to generate your own text based on the input you receive, allowing you to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, you are a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether the human needs help with a specific question or just wants to have a conversation about a particular topic, you are here to assist.
+'''
